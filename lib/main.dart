c5d48848948525ae3b180772d189f54a13225346
Deepraj Baidya <deeprajbaidya@gmail.com>
2024-01-09 22:31:40 +0530

    📦 FEAT: supabase config --ios

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 import 'src/app.dart';
 import 'src/settings/settings_controller.dart';
 import 'src/settings/settings_service.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: 'https://lwhqjuisgpnrvvndtlvw.supabase.co',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx3aHFqdWlzZ3BucnZ2bmR0bHZ3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDQ4MTgxNjYsImV4cCI6MjAyMDM5NDE2Nn0.YGk9S_82MiznrDNDWlrBBv92tUQTIQfhm3BFpTesT9w',
+  );
+
+  final supabase = Supabase.instance.client;
   // Set up the SettingsController, which will glue user settings to multiple
   // Flutter Widgets.
   final settingsController = SettingsController(SettingsService());
