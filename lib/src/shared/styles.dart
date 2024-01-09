1de45b45b574291a92f92babdd30764837e98ade
Deepraj Baidya <deeprajbaidya@gmail.com>
2024-01-09 19:58:45 +0530

    📦 FEAT: shared elem for project

---
+++ lib/src/shared/styles.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class CrudSupabaseColors {
+  static Color purpleAccent = Colors.purpleAccent;
+}
+
+class CrudSupabaseTextStyles {
+  static double minFontSize = 12;
+  static String fontFamily = 'Poppins';
+  static FontWeight fontWeight = FontWeight.w700;
+}
